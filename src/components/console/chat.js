d7ea079887268a67249cd7457d059ea0d42b30ad
rhmtrz <2249301106@qq.com>
2018-09-02 20:58:26 +0900

    feat: send and get data firebase

--- src/components/console/chat.js
+++ src/components/console/chat.js
@@ -1,68 +1,124 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase'; 
-require("firebase/firestore")
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { connect } from 'react-redux';
 import {
-  StyleSheet, Text, View, Button
+  View, Text, TextInput, Button, Alert, Image,
 } from 'react-native';
-import { connect } from "react-redux";
-import { onSaveDataToDB } from "../redux/reducer/pageDataReducer"; 
 
-
-const settings = {timestampsInSnapshots: true};
-
-firebase.initializeApp(ApiKeys.config)
-firebase.firestore().settings(settings)
-
-const db = firebase.firestore();
+import * as firebase from 'firebase';
 
 class ChatRoomComponent extends React.Component {
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
-      message: '', 
-      data: []
-    }
+      msg: '',
+      data: [],
+    };
 
- 
-
-    this.onClick = (n) => {
-      db.collection("users").add({
-        first: "rah",
-        last: "hoge",
-        born: n
-      }).then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
+    this.onSubmitButtonPressed = () => {
+      const db = firebase.firestore();
+      db.settings({
+        timestampsInSnapshots: true,
       });
-    }
+      const collection = db.collection('messages');
+      const { msg } = this.state;
+      collection.add({
+        message: msg,
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+        .then((resolve) => {
+          Alert.alert('Success to add collection!');
+          // eslint-disable-next-line no-console
+          console.log(resolve);
+        });
+    };
+
+    this.getData = () => {
+      const db = firebase.firestore();
+      db.settings({
+        timestampsInSnapshots: true,
+      });
+      const collection = db.collection('messages');
+      collection.get()
+        .then((resolve) => {
+          this.setState({ data: resolve.docs });
+        });
+    };
+
+    this.getFewData = () => {
+      const db = firebase.firestore();
+      db.settings({
+        timestampsInSnapshots: true,
+      });
+      const collection = db.collection('posts');
+      collection.where('pinLng', '>', 120).where('pinLng', '<', 130).get()
+        .then((resolve) => {
+          this.setState({ data: resolve.docs });
+        })
+        .catch((error) => {
+          Alert.alert(error);
+        });
+    };
+
+    this.onClearButtonPressed = () => {
+      this.setState({ data: [] });
+    };
   }
 
   render() {
-    const arr = [1, 10, 100, 1000];
+    const { msg, data } = this.state;
     return (
       <View>
-        <View>
-          <Text onPress={(e) => this.onClick(e)}>Submit</Text>
-        </View>
+        <Text>
+          simple chat feature comming soon!
+        </Text>
+        <TextInput
+          value={msg}
+          onChangeText={text => this.setState({ msg: text })}
+          style={{ backgroundColor: 'white', width: 300 }}
+        />
+        <Button
+          title="Submit"
+          onPress={this.onSubmitButtonPressed}
+        />
+        <Button
+          title="Clear"
+          onPress={this.onClearButtonPressed}
+        />
+        <Button
+          title="get posts data!"
+          onPress={this.getData}
+        />
+        <Button
+          title="get a few data"
+          onPress={this.getFewData}
+        />
+        {data.map(d => (
+          <View key={d.id}>
+            <Text>
+              {/* {d.data().uid} */}
+            </Text>
+            <Image
+              source={{ uri: d.data().photoUrl }}
+              style={{ width: 100, height: 100 }}
+            />
+          </View>
+        ))}
       </View>
     );
   }
-}  
+}
 
+// const mapStateTpProps = (state) => {
+//   const { uid } = state.pageData;
+//   return {
+//     ...state, uid,
+//   };
+// };
 
-const mapStateToProps = state => {
-  const { message } = state.chatData;
-  return {
-    message
-  };
+ChatRoomComponent.propTypes = {
+  //uid: PropTypes.string.isRequired,
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onSaveDataToDB: () => dispatch(onSaveDataToDB())
-  };
-};
+export default ChatRoomComponent;
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChatRoomComponent);
--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,11 +1,12 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducer";
+import middlewares from './middlewares';
 
 const store = createStore(
   reducers,
   {},
-  composeWithDevTools(),
+  composeWithDevTools(middlewares),
 );
 
 export default function configureStore() {
