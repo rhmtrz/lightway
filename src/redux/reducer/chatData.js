6a6a25bf076975057ac4873a1d90e9db09e13919
rhmtrz <2249301106@qq.com>
2018-09-06 09:24:58 +0900

    feat: fblogin by redux and middleware

--- src/redux/reducer/chatData.js
+++ src/redux/reducer/chatData.js
@@ -1,18 +1,18 @@
 import { createAction } from "redux-actions";
 
-export const TOGGLE_USER_PROFILE_TAB = "TOGGLE_USER_PROFILE_TAB";
-export const onClickedUserProfileTab = createAction(TOGGLE_USER_PROFILE_TAB);
+export const SAVE_DATA_TO_DB = "SAVE_DATA_TO_DB";
+export const onSaveDataToDB = createAction(SAVE_DATA_TO_DB);
 
 const INITIAL_STATE = {
-  toggleTab: "liked",
+  message: "",
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case TOGGLE_USER_PROFILE_TAB: {
+  case SAVE_DATA_TO_DB: {
     return {
       ...state,
-      toggleTab: action.payload,
+      message: action.payload,
     };
   }
   default:
